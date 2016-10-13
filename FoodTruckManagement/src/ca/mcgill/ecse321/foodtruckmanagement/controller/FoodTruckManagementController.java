2dfa336032a5580001e4df8b7e7b3c30184942d2
Jordan Itzkovitz <jordan.itzkovitz@mail.mcgill.ca>
2016-10-13 17:32:31 -0400

    Fixed Controller to edit already existing supplies rather than adding new objects each time

--- FoodTruckManagement/src/ca/mcgill/ecse321/foodtruckmanagement/controller/FoodTruckManagementController.java
+++ FoodTruckManagement/src/ca/mcgill/ecse321/foodtruckmanagement/controller/FoodTruckManagementController.java
@@ -1,5 +1,7 @@
 package ca.mcgill.ecse321.foodtruckmanagement.controller;
 
+import java.util.List;
+
 import ca.mcgill.ecse321.foodtruckmanagement.model.FoodSupply;
 import ca.mcgill.ecse321.foodtruckmanagement.model.FoodTruckManager;
 import ca.mcgill.ecse321.foodtruckmanagement.persistence.PersistenceXStream;
@@ -18,12 +20,24 @@ public class FoodTruckManagementController {
 		if(amount <= 0)
 			throw new InvalidInputException("Supply amount must be greater than zero!");
 		
-		FoodSupply f = new FoodSupply(name,amount);
 		FoodTruckManager fm = FoodTruckManager.getInstance();
 		
-		fm.addFoodSupply(f);
+		for(int i = 0; i < fm.numberOfFoodSupplies(); i++)
+		{
+			if(fm.getFoodSupply(i).getName().toUpperCase().equals(name.toUpperCase()))
+			{
+				amount = fm.getFoodSupply(i).getAmount() + amount;
+				fm.getFoodSupply(i).setAmount(amount);
+				PersistenceXStream.saveToXMLwithXStream(fm);
+				return;
+			}
+		}
 		
+		FoodSupply f = new FoodSupply(name,amount);
+		fm.addFoodSupply(f);
 		PersistenceXStream.saveToXMLwithXStream(fm);
+		
+		
 	}
 	
 }
