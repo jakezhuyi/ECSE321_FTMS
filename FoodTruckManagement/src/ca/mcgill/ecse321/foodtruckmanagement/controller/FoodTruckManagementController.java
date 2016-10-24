3d97ce9131a4740cc4aef94a90dff6bcdbf96b32
nlevine21 <noahlevine21@gmail.com>
2016-10-24 15:04:45 -0400

    View schedule for the next two weeks

--- FoodTruckManagement/src/ca/mcgill/ecse321/foodtruckmanagement/controller/FoodTruckManagementController.java
+++ FoodTruckManagement/src/ca/mcgill/ecse321/foodtruckmanagement/controller/FoodTruckManagementController.java
@@ -323,46 +323,18 @@ public class FoodTruckManagementController {
 		c.set(Calendar.DAY_OF_WEEK, c.getFirstDayOfWeek());
 		
 		int month = c.get(Calendar.MONTH);
-		String monthString = getMonth(month);
-		
 		int day = c.get(Calendar.DAY_OF_MONTH);
 		int year = c.get(Calendar.YEAR);
 
 
-		String scheduleList = "<html><h3 style=\"text-align:center;\">" + e.getName() + "'s Schedule for Week of " + monthString + " " + day + ", " + year + "</h3></title>";
+		String scheduleList = calculateSched(c, e, day, month, year);
+		month = c.get(Calendar.MONTH);
+		day = c.get(Calendar.DAY_OF_MONTH);
+		year = c.get(Calendar.YEAR);
 		
-		scheduleList = scheduleList + "<table><tr><th><u>Monday</u></th><th><u>Tuesday</u></th><th><u>Wednesday</u></th><th><u>Thursday</u></th>"
-									+ "<th><u>Friday</u></th><th><u>Saturday</u></th><th><u>Sunday</u></th></tr>";
+		scheduleList = scheduleList + calculateSched(c, e, day, month, year);
 		
-		scheduleList = scheduleList + "<tr>";
-		
-		//Loop through all the days of the week and check the employee's schedule
-		for (int i=0; i<7; i++)
-		{
-			Date date = new Date(c.getTimeInMillis());
-			scheduleList = scheduleList + "<td>";
-			
-			if (e.numberOfSchedules() == 0)
-				scheduleList = scheduleList + "<i>Not Scheduled</i>";
-
-			for (int j=0; j<e.numberOfSchedules(); j++)
-			{				
-				Schedule employeeSchedule = e.getSchedule(j);
-				
-				if(employeeSchedule.getWorkDay().toString().equals(date.toString()))
-				{
-					scheduleList = scheduleList + employeeSchedule.getStartTime().toString().substring(0,5) + " - " + 
-									employeeSchedule.getEndTime().toString().substring(0, 5);
-					break;
-				}
-				if (j==e.numberOfSchedules()-1)
-					scheduleList = scheduleList + "<i>Not Scheduled</i>";
-			}
-			scheduleList = scheduleList + "</td>";
-			c.add(Calendar.DAY_OF_WEEK, 1);
-		}
-		
-		scheduleList = scheduleList + "</tr></table></html>";
+		scheduleList = scheduleList + "</html>";
 		
 		return scheduleList;
 		
@@ -419,6 +391,45 @@ public class FoodTruckManagementController {
 	private static String getMonth(int month){
 	    String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 	    return monthNames[month];
+	}
+	
+	private static String calculateSched(Calendar c, Employee e, int day, int month, int year) {
+		String scheduleList = "<html><h3 style=\"text-align:center;\">" + e.getName() + "'s Schedule for Week of " + getMonth(month) + " " + day + ", " + year + "</h3></title>";
+		
+		scheduleList = scheduleList + "<table><tr><th><u>Monday</u></th><th><u>Tuesday</u></th><th><u>Wednesday</u></th><th><u>Thursday</u></th>"
+									+ "<th><u>Friday</u></th><th><u>Saturday</u></th><th><u>Sunday</u></th></tr>";
+		
+		scheduleList = scheduleList + "<tr>";
+		
+		//Loop through all the days of the week and check the employee's schedule
+		for (int i=0; i<7; i++)
+		{
+			Date date = new Date(c.getTimeInMillis());
+			scheduleList = scheduleList + "<td>";
+			
+			if (e.numberOfSchedules() == 0)
+				scheduleList = scheduleList + "<i>Not Scheduled</i>";
+
+			for (int j=0; j<e.numberOfSchedules(); j++)
+			{				
+				Schedule employeeSchedule = e.getSchedule(j);
+				
+				if(employeeSchedule.getWorkDay().toString().equals(date.toString()))
+				{
+					scheduleList = scheduleList + employeeSchedule.getStartTime().toString().substring(0,5) + " - " + 
+									employeeSchedule.getEndTime().toString().substring(0, 5);
+					break;
+				}
+				if (j==e.numberOfSchedules()-1)
+					scheduleList = scheduleList + "<i>Not Scheduled</i>";
+			}
+			scheduleList = scheduleList + "</td>";
+			c.add(Calendar.DAY_OF_WEEK, 1);
+		}
+		
+		scheduleList = scheduleList + "</tr></table><br/><br/>";
+		
+		return scheduleList;
 	}
 	
 	
