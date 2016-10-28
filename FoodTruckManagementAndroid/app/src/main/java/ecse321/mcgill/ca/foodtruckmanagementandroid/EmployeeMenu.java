01769930daf7a923bfb04e6201cff68083c45fa8
spiro mavroidakos <spiro mavroidakos>
2016-10-27 23:57:17 -0400

    Finished add/remove supply/equipment and add employee and some new activities for android

--- FoodTruckManagementAndroid/app/src/main/java/ecse321/mcgill/ca/foodtruckmanagementandroid/EmployeeMenu.java
+++ FoodTruckManagementAndroid/app/src/main/java/ecse321/mcgill/ca/foodtruckmanagementandroid/EmployeeMenu.java
@@ -7,8 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import android.widget.TextView;
 
 import java.io.File;
@@ -18,20 +17,18 @@ import ca.mcgill.ecse321.foodtruckmanagement.controller.InvalidInputException;
 import ca.mcgill.ecse321.foodtruckmanagement.model.FoodTruckManager;
 import ca.mcgill.ecse321.foodtruckmanagement.persistence.PersistenceFoodTruckManagement;
 
-public class MainActivity extends AppCompatActivity {
-
-    public FoodTruckManager ftm;
+public class EmployeeMenu extends AppCompatActivity {
+    private FoodTruckManager ftm;
     private String error = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_employee_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         PersistenceFoodTruckManagement.setFilename(getFilesDir().getAbsolutePath() + File.pathSeparator +"foodtruckmanagement.xml");
-        System.out.println(getFilesDir().getAbsolutePath() + "foodtruckmanagement.xml");
         PersistenceFoodTruckManagement.loadFoodTruckManagementModel();
 
         ftm = FoodTruckManager.getInstance();
@@ -46,33 +43,35 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void employeeMenu(View v){
-        startActivity(new Intent(MainActivity.this, EmployeeMenu.class));
-    }
-    public void supplyMenu(View v){
-        startActivity(new Intent(MainActivity.this, SupplyMenu.class));
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void refreshData(){
+        TextView tv;
+        tv = (TextView) findViewById(R.id.employee_name);
+        tv.setText("");
+        tv = (TextView) findViewById(R.id.employee_role);
+        displayError(tv);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void displayError(TextView tv){
+        TextView e = (TextView) findViewById(R.id.error_name);
+        tv.setText("");
+        if (error != null){
+            //message_error
+            e.setText(error);
+        }else {
+            e.setText("");
         }
-
-        return super.onOptionsItemSelected(item);
     }
+    public void addEmployee(View v){
+        TextView tv = (TextView) findViewById(R.id.employee_name);
+        TextView tv2 = (TextView) findViewById(R.id.employee_role);
+        FoodTruckManagementController ftc = new FoodTruckManagementController();
+        error = null;
+        try {
+            ftc.addEmployee(tv.getText().toString(), tv2.getText().toString());
+        }catch (InvalidInputException e){
+            error = e.getMessage();
+        }
+        refreshData();
+    }
+
 }
